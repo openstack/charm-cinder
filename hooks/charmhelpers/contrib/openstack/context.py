b77a2d631ce20c13f9eae74da9f9929b4dcf3757
yolanda.robla@canonical.com <>
2014-01-08 11:13:13 +0100

    refreshed charmhelpers

--- hooks/charmhelpers/contrib/openstack/context.py
+++ hooks/charmhelpers/contrib/openstack/context.py
@@ -182,11 +182,11 @@ class AMQPContext(OSContextGenerator):
                     # Sufficient information found = break out!
                     break
             # Used for active/active rabbitmq >= grizzly
-            if relation_get('vip', rid=rid, unit=unit) is None:
+            if 'clustered' not in ctxt and len(related_units(rid))>0:
                 rabbitmq_hosts = []
                 for unit in related_units(rid):
                     rabbitmq_hosts.append(relation_get('private-address',
-                                                           rid=rid, unit=unit))
+                                                       rid=rid, unit=unit))
                 ctxt['rabbitmq_hosts'] = ','.join(rabbitmq_hosts)
         if not context_complete(ctxt):
             return {}
@@ -435,25 +435,55 @@ class NeutronContext(object):
 
 
 class OSConfigFlagContext(OSContextGenerator):
-        '''
-        Responsible adding user-defined config-flags in charm config to a
-        to a template context.
-        '''
+        """
+        Responsible for adding user-defined config-flags in charm config to a
+        template context.
+
+        NOTE: the value of config-flags may be a comma-separated list of
+              key=value pairs and some Openstack config files support
+              comma-separated lists as values.
+        """
         def __call__(self):
             config_flags = config('config-flags')
-            if not config_flags or config_flags in ['None', '']:
+            if not config_flags:
                 return {}
-            config_flags = config_flags.split(',')
+
+            if config_flags.find('==') >= 0:
+                log("config_flags is not in expected format (key=value)",
+                    level=ERROR)
+                raise OSContextError
+
+            # strip the following from each value.
+            post_strippers = ' ,'
+            # we strip any leading/trailing '=' or ' ' from the string then
+            # split on '='.
+            split = config_flags.strip(' =').split('=')
+            limit = len(split)
             flags = {}
-            for flag in config_flags:
-                if '=' not in flag:
-                    log('Improperly formatted config-flag, expected k=v '
-                        'got %s' % flag, level=WARNING)
-                    continue
-                k, v = flag.split('=')
-                flags[k.strip()] = v
-            ctxt = {'user_config_flags': flags}
-            return ctxt
+            for i in xrange(0, limit - 1):
+                current = split[i]
+                next = split[i + 1]
+                vindex = next.rfind(',')
+                if (i == limit - 2) or (vindex < 0):
+                    value = next
+                else:
+                    value = next[:vindex]
+
+                if i == 0:
+                    key = current
+                else:
+                    # if this not the first entry, expect an embedded key.
+                    index = current.rfind(',')
+                    if index < 0:
+                        log("invalid config value(s) at index %s" % (i),
+                            level=ERROR)
+                        raise OSContextError
+                    key = current[index + 1:]
+
+                # Add to collection.
+                flags[key.strip(post_strippers)] = value.rstrip(post_strippers)
+
+            return {'user_config_flags': flags}
 
 
 class SubordinateConfigContext(OSContextGenerator):
