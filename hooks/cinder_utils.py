f23828cd4b3fce1dd81d2c5ca8fe045e6a4dd08c
Kapil Thangavelu <kapil@canonical.com>
2014-02-21 07:37:46 -0500

    ssl support

--- hooks/cinder_utils.py
+++ hooks/cinder_utils.py
@@ -84,8 +84,9 @@ CLUSTER_RES = 'res_cinder_vip'
 class CinderCharmError(Exception):
     pass
 
-CINDER_CONF = '/etc/cinder/cinder.conf'
-CINDER_API_CONF = '/etc/cinder/api-paste.ini'
+CINDER_CONF_DIR = "/etc/cinder"
+CINDER_CONF = '%s/cinder.conf' % CINDER_CONF_DIR
+CINDER_API_CONF = '%s/api-paste.ini' % CINDER_CONF_DIR
 CEPH_CONF = '/etc/ceph/ceph.conf'
 HAPROXY_CONF = '/etc/haproxy/haproxy.cfg'
 APACHE_SITE_CONF = '/etc/apache2/sites-available/openstack_https_frontend'
@@ -97,7 +98,7 @@ TEMPLATES = 'templates/'
 # with file in restart_on_changes()'s service map.
 CONFIG_FILES = OrderedDict([
     (CINDER_CONF, {
-        'hook_contexts': [context.SharedDBContext(),
+        'hook_contexts': [context.SharedDBContext(ssl_dir=CINDER_CONF_DIR),
                           context.AMQPContext(),
                           context.ImageServiceContext(),
                           context.OSConfigFlagContext(),
