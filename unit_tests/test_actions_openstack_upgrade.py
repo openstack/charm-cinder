8d408ed8276b01c6792a855f125a3a8918dfe0e3
Corey Bryant <corey.bryant@canonical.com>
2015-09-15 18:29:41 +0000

    Update unit tests

--- unit_tests/test_actions_openstack_upgrade.py
+++ unit_tests/test_actions_openstack_upgrade.py
@@ -12,7 +12,7 @@ from test_utils import (
 
 TO_PATCH = [
     'config_changed',
-    'do_action_openstack_upgrade',
+    'do_openstack_upgrade',
     'register_configs',
     'relation_set',
     'relation_ids',
@@ -28,14 +28,19 @@ class TestCinderUpgradeActions(CharmTestCase):
 
     @patch('charmhelpers.contrib.openstack.utils.config')
     @patch('charmhelpers.contrib.openstack.utils.action_set')
-    def test_openstack_upgrade_true(self, action_set, config):
-        self.do_action_openstack_upgrade.return_value = True
+    @patch('charmhelpers.contrib.openstack.utils.git_install_requested')
+    @patch('charmhelpers.contrib.openstack.utils.openstack_upgrade_available')
+    def test_openstack_upgrade_true(self, upgrade_avail, git_requested,
+                                    action_set, config):
+        git_requested.return_value = False
+        upgrade_avail.return_value = True
+        config.return_value = True
         self.relation_ids.return_value = ['relid1']
         self.uuid.uuid4.return_value = 12345
 
         openstack_upgrade.openstack_upgrade()
 
-        self.assertTrue(self.do_action_openstack_upgrade.called)
+        self.assertTrue(self.do_openstack_upgrade.called)
         self.assertTrue(self.relation_ids.called)
         self.relation_set.assert_called_with(relation_id='relid1',
                                              upgrade_nonce=12345)
@@ -43,12 +48,17 @@ class TestCinderUpgradeActions(CharmTestCase):
 
     @patch('charmhelpers.contrib.openstack.utils.config')
     @patch('charmhelpers.contrib.openstack.utils.action_set')
-    def test_openstack_upgrade_false(self, action_set, config):
-        self.do_action_openstack_upgrade.return_value = False
+    @patch('charmhelpers.contrib.openstack.utils.git_install_requested')
+    @patch('charmhelpers.contrib.openstack.utils.openstack_upgrade_available')
+    def test_openstack_upgrade_true(self, upgrade_avail, git_requested,
+                                    action_set, config):
+        git_requested.return_value = False
+        upgrade_avail.return_value = True
+        config.return_value = False
 
         openstack_upgrade.openstack_upgrade()
 
-        self.assertTrue(self.do_action_openstack_upgrade.called)
+        self.assertFalse(self.do_openstack_upgrade.called)
         self.assertFalse(self.relation_ids.called)
         self.assertFalse(self.relation_set.called)
         self.assertFalse(self.config_changed.called)
