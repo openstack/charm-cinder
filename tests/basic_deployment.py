e6100d9dd516030e0599cd0499b295b26cfdb136
Ryan Beisner <ryan.beisner@canonical.com>
2016-05-10 21:05:39 +0000

    Use bundletester for amulet test execution
    Switch to using bundletester for execution of functional tests,
    leveraging tox to build out test virtualenvs.
    
    Rename amulet tests inline with gate-*, dev-* and dfs-*
    naming standards.
    
    Update README to refer to functional testing section of the charm
    guide.
    
    Change-Id: I6d9582386b77cae8a00726760dc7cead85af7527

--- tests/basic_deployment.py
+++ tests/basic_deployment.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #
 # Copyright 2016 Canonical Ltd
 #
@@ -131,11 +131,11 @@ class CinderBasicDeployment(OpenStackAmuletDeployment):
     def _initialize_tests(self):
         """Perform final initialization before tests get run."""
         # Access the sentries for inspecting service units
-        self.cinder_sentry = self.d.sentry.unit['cinder/0']
-        self.glance_sentry = self.d.sentry.unit['glance/0']
-        self.mysql_sentry = self.d.sentry.unit['mysql/0']
-        self.keystone_sentry = self.d.sentry.unit['keystone/0']
-        self.rabbitmq_sentry = self.d.sentry.unit['rabbitmq-server/0']
+        self.cinder_sentry = self.d.sentry['cinder'][0]
+        self.glance_sentry = self.d.sentry['glance'][0]
+        self.mysql_sentry = self.d.sentry['mysql'][0]
+        self.keystone_sentry = self.d.sentry['keystone'][0]
+        self.rabbitmq_sentry = self.d.sentry['rabbitmq-server'][0]
         u.log.debug('openstack release val: {}'.format(
             self._get_openstack_release()))
         u.log.debug('openstack release str: {}'.format(
@@ -774,16 +774,23 @@ class CinderBasicDeployment(OpenStackAmuletDeployment):
     def test_910_pause_and_resume(self):
         """The services can be paused and resumed. """
         u.log.debug('Checking pause and resume actions...')
-        unit_name = "cinder/0"
-        unit = self.d.sentry.unit[unit_name]
+        unit = self.d.sentry['cinder'][0]
+        unit_name = unit.info['unit_name']
 
+        u.log.debug('Checking for active status on {}'.format(unit_name))
         assert u.status_get(unit)[0] == "active"
 
+        u.log.debug('Running pause action on {}'.format(unit_name))
         action_id = self._run_action(unit_name, "pause")
+        u.log.debug('Waiting on action {}'.format(action_id))
         assert self._wait_on_action(action_id), "Pause action failed."
+        u.log.debug('Checking for maintenance status on {}'.format(unit_name))
         assert u.status_get(unit)[0] == "maintenance"
 
+        u.log.debug('Running resume action on {}'.format(unit_name))
         action_id = self._run_action(unit_name, "resume")
+        u.log.debug('Waiting on action {}'.format(action_id))
         assert self._wait_on_action(action_id), "Resume action failed."
+        u.log.debug('Checking for active status on {}'.format(unit_name))
         assert u.status_get(unit)[0] == "active"
         u.log.debug('OK')
