734fb80b08e3fba6249ce911baa068a8eda65a7a
Alex Kavanagh <alex@ajkavanagh.co.uk>
2016-06-15 13:10:31 +0000

    Fix for multiple status-set - related to bug 1588462
    This change fixes the obvious race for a status_set() between
    check_optional_interfaces() and assess_status() as the later calls the former
    which calls status_set(), returns the status, which is then potentially set
    again by the assess_status() function.  This cleans up the code so that only a
    single status_set() is performed when calling assess_status().
    
    Change-Id: I3604ed3b36af91afb95d43e0e5e24483c9919fd1
    Related-Bug:#1588462

--- unit_tests/test_cinder_utils.py
+++ unit_tests/test_cinder_utils.py
@@ -966,6 +966,7 @@ class TestCinderUtils(CharmTestCase):
             asf.assert_called_once_with('test-config')
             callee.assert_called_once_with()
 
+    @patch.object(cinder_utils, 'get_optional_interfaces')
     @patch.object(cinder_utils, 'check_optional_relations')
     @patch.object(cinder_utils, 'REQUIRED_INTERFACES')
     @patch.object(cinder_utils, 'services')
@@ -974,12 +975,16 @@ class TestCinderUtils(CharmTestCase):
                                 make_assess_status_func,
                                 services,
                                 REQUIRED_INTERFACES,
-                                check_optional_relations):
+                                check_optional_relations,
+                                get_optional_interfaces):
         services.return_value = 's1'
+        REQUIRED_INTERFACES.copy.return_value = {'int': ['test 1']}
+        get_optional_interfaces.return_value = {'opt': ['test 2']}
         cinder_utils.assess_status_func('test-config')
         # ports=None whilst port checks are disabled.
         make_assess_status_func.assert_called_once_with(
-            'test-config', REQUIRED_INTERFACES,
+            'test-config',
+            {'int': ['test 1'], 'opt': ['test 2']},
             charm_func=check_optional_relations,
             services='s1', ports=None)
 
