a956c6b9d87ee43c769b2669569b46efbaa23df1
Liam Young <liam.young@canonical.com>
2017-12-15 07:16:52 +0000

    Request class-read object_prefix rbd_children perm
    When using ceph as a backend request the additional privilege
    class-read on rbd_children. This fixes bug 1696073.
    
    Change-Id: Ia5f092255f1ff75796fc24a8bbd94dd1831e6807
    Closes-Bug: #1696073
    Depends-On: Icf844ec7d33f2e558dee7935fe5fa3d7f08e0d59

--- hooks/cinder_hooks.py
+++ hooks/cinder_hooks.py
@@ -416,12 +416,18 @@ def get_ceph_request():
                           replica_count=replicas,
                           group="volumes")
     if config('restrict-ceph-pools'):
-        rq.add_op_request_access_to_group(name="volumes",
-                                          permission='rwx')
-        rq.add_op_request_access_to_group(name="images",
-                                          permission='rwx')
-        rq.add_op_request_access_to_group(name="vms",
-                                          permission='rwx')
+        rq.add_op_request_access_to_group(
+            name="volumes",
+            object_prefix_permissions={'class-read': ['rbd_children']},
+            permission='rwx')
+        rq.add_op_request_access_to_group(
+            name="images",
+            object_prefix_permissions={'class-read': ['rbd_children']},
+            permission='rwx')
+        rq.add_op_request_access_to_group(
+            name="vms",
+            object_prefix_permissions={'class-read': ['rbd_children']},
+            permission='rwx')
     return rq
 
 
